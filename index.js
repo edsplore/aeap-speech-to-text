a55e4f227cd538721bf6b91b5bb62d90a0887297
edsplore <138727039+edsplore@users.noreply.github.com>
2024-04-19 08:31:40 +0530

    revert back to queue fix

--- index.js
+++ index.js
@@ -7,7 +7,8 @@
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,42 +19,43 @@
 
 const { Codecs } = require("./lib/codecs");
 const { Languages } = require("./lib/languages");
-const { getProvider }  = require("./lib/provider"); // Ensure this is correctly pointing to your updated gRPC provider
+
+const { getProvider } = require("./lib/provider");
 const { getServer } = require("./lib/server");
 const { dispatch } = require("./lib/dispatcher");
 
 const argv = require("yargs/yargs")(process.argv.slice(2))
-    .command("$0 [options]", "Start a speech to text server", {
-        port: {
-            alias: "p",
-            desc: "Port to listen on",
-            default: 9099,
-            type: "number",
-            group: "Server",
-        },
-    })
-    .strict()
-    .argv;
+	.command("$0 [options]", "Start a speech to text server", {
+		port: {
+			alias: "p",
+			desc: "Port to listen on",
+			default: 9099,
+			type: "number",
+			group: "Server",
+		},
+	})
+	.strict()
+	.argv;
 
 const codecs = new Codecs(argv);
 const languages = new Languages(argv);
 const server = getServer("ws", argv);
 
 server.on("connection", (client) => {
-    dispatch({
-        codecs: codecs,
-        languages: languages,
-        transport: client,
-        provider: getProvider("custom", {serverAddress: 'localhost:8084'}),
-    });
+	dispatch({
+		codecs: codecs,
+		languages: languages,
+		transport: client,
+		provider: getProvider("custom", argv),
+	});
 });
 
 process.on("SIGINT", () => {
-    server.close();
-    process.exit(0);
+	server.close();
+	process.exit(0);
 });
 
 process.on("SIGTERM", () => {
-    server.close();
-    process.exit(0);
+	server.close();
+	process.exit(0);
 });
